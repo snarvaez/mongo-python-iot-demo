1db5468d6b9d0020a4719047748bb1d73ce2bf3e
Sigfrido Narváez <sigfrido@gmail.com>
2018-08-03 18:19:49 -0700

    Scripts and code

---
+++ IOT_BulkInsertWithRetry.py
@@ -0,0 +1,40 @@
+import pymongo, json
+import sys, time, datetime
+
+# ================
+# DB IOT
+# COLL DeviceData, Capped = 2102400 2TB
+# ================
+connStr= sys.argv[1]
+dbName= sys.argv[2]     # IOT
+collName= sys.argv[3]   # IOT_DeviceData
+
+client= pymongo.MongoClient(connStr, retryWrites=True)
+db= client[dbName]
+coll= db[collName]
+
+bulk= []
+while 1:
+   try:
+       post= sys.stdin.readline()
+   except KeyboardInterrupt:
+       break
+   if not post:
+       break
+
+   post= post.replace("$date","date")
+   j= json.loads(post)
+   bulk.append(pymongo.InsertOne(j))
+
+for retry in [3, 5, 7, 13, 17]: #exponential back-off & retry
+    try:
+        result= coll.bulk_write(bulk)
+        print(result)
+        break
+
+    except Exception as e: #pymongo.errors.BulkWriteError
+        print("EXCEPTION: ", e)
+        print("Sleeping for ", retry, " seconds and then retrying...")
+        time.sleep(retry)
+
+client.close()
